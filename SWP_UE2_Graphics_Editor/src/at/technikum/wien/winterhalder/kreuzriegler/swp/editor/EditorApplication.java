21cf90e67a3263dc6928692049e34072a306a403
Matthias Kreuzriegler <matthias.kreuzriegler@gmx.at>
2014-04-18 14:16:30 +0200

    init

---
+++ SWP_UE2_Graphics_Editor/src/at/technikum/wien/winterhalder/kreuzriegler/swp/editor/EditorApplication.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package at.technikum.wien.winterhalder.kreuzriegler.swp.editor;
+
+/**
+ * @author Matthias
+ *
+ */
+public class EditorApplication {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
